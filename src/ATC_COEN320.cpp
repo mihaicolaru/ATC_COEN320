d63393ece559b80b4a71d4e51cdeeeb3f71bd02a
JeffZhang852 <zhangjeff219@gmail.com>
2022-11-09 16:55:28 -0500

    80% finish display system, require fix for loop timer

--- src/ATC_COEN320.cpp
+++ src/ATC_COEN320.cpp
@@ -13,43 +13,45 @@
 #include "Display.h"
 
 int main() {
+//	time_t at;
+//	time_t et;
+	ComputerSystem atc;
 
-    //ComputerSystem atc;
-
-    //atc.stop();
-	Display testDisp;
-
+	atc.stop();
 
 //	int arrivalTime1 = 3;
 //	int ID1 = 1;
 //	int position1[3] = {90000, 90000, 15000};
 //	int speed1[3] = {1000, 1000, -1000};
-
-//	int arrivalTime2 = 1;
-//	int ID2 = 2;
-//	int position2[3] = {90000, 90000, 15000};
-//	int speed2[3] = {2000, 500, 1000};
 //
-//	int arrivalTime3 = 4;
-//	int ID3 = 3;
-//	int position3[3] = {90000, 90000, 15000};
-//	int speed3[3] = {1500, 1400, 950};
+//	//	int arrivalTime2 = 1;
+//	//	int ID2 = 2;
+//	//	int position2[3] = {90000, 90000, 15000};
+//	//	int speed2[3] = {2000, 500, 1000};
+//	//
+//	//	int arrivalTime3 = 4;
+//	//	int ID3 = 3;
+//	//	int position3[3] = {90000, 90000, 15000};
+//	//	int speed3[3] = {1500, 1400, 950};
+//	//
+//	//	int arrivalTime4 = 1;
+//	//	int ID4 = 4;
+//	//	int position4[3] = {90000, 90000, 15000};
+//	//	int speed4[3] = {2500, 400, 2000};
 //
-//	int arrivalTime4 = 1;
-//	int ID4 = 4;
-//	int position4[3] = {90000, 90000, 15000};
-//	int speed4[3] = {2500, 400, 2000};
-
-
+//	time (&at);
 //	Plane plane1(arrivalTime1, ID1, position1, speed1);
-//	Plane plane2(arrivalTime2, ID2, position2, speed2);
-//	Plane plane3(arrivalTime3, ID3, position3, speed3);
-//	Plane plane4(arrivalTime4, ID4, position4, speed4);
-
+//	//	Plane plane2(arrivalTime2, ID2, position2, speed2);
+//	//	Plane plane3(arrivalTime3, ID3, position3, speed3);
+//	//	Plane plane4(arrivalTime4, ID4, position4, speed4);
+//
 //	plane1.stop();
-//	plane2.stop();
-//	plane3.stop();
-//	plane4.stop();
+//	time (&et);
+//	double exe = difftime(et,at);
+//	std::cout << "finished in: " << exe;
+	//	plane2.stop();
+	//	plane3.stop();
+	//	plane4.stop();
 
 	return 0;
 }
--- src/Display.h
+++ src/Display.h
@@ -8,54 +8,138 @@
 #ifndef DISPLAY_H_
 #define DISPLAY_H_
 
-#define SCALER 10000
-
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
+#include <pthread.h>
+#include <iostream>
+#include <mutex>
+#include <errno.h>
+#include <sys/siginfo.h>
+#include <sys/neutrino.h>
 
-#include "Plane.h"
+#define SCALER 3000
+#define MARGIN 100000
+#define PERIOD_D 5000000 //5sec period
+
+const int block_count = (int)MARGIN/(int)SCALER;
+
+//need sporadic task?
+
+
+
 //Add vector source library, should be computer system
 
 class Display{
 public:
 	//constructor
+	//int _posX[], int _posY[], int _posZ[]
 	Display(){
-		test();
+
+		// set thread in detached state
+		int rc = pthread_attr_init(&attrDisp);
+		if (rc){
+			printf("ERROR, RC from pthread_attr_init() is %d \n", rc);
+		}
+
+		rc = pthread_attr_setdetachstate(&attrDisp, PTHREAD_CREATE_JOINABLE);
+		if (rc){
+			printf("ERROR; RC from pthread_attr_setdetachstate() is %d \n", rc);
+		}
+
+		start();
 	}
 	//destructor
 	~Display(){
 
 	}
 
+	bool start(){
+		std::cout << "Start display function\n";
+		time(&at);
+		return (pthread_create(&displayThread, &attrDisp, updateStart, this) == 0);
+	}
+
+	bool stop(){
+		pthread_join(displayThread, NULL);
+		return 0;
+	}
+
+	static void *updateStart(void *context){
+		return ((Display *)context)->updateDisplay();
+	}
+
+	void *updateDisplay(void){
+		//		printf("update display called\n");
+		int chid =ChannelCreate(0);
+		if(chid == -1){
+			std::cout << "couldn't create channel!\n";
+		}
+
+		Timer timer(chid);
+		timer.setTimer( 1000000, PERIOD_D);
+		printMap();
+
+		time(&et);
+		double exe = difftime(et,at);
+		std::cout << "DISPLAY finished in: " << exe << std::endl;
+
+		ChannelDestroy(chid);
+		return 0;
+
+	}
+
 
 private:
-	int posX[5] = {10000,12000,30000,20000,70000};
-	int posY[5] = {10000,12000,7000, 50000, 90000};
+	time_t at;
+	time_t et;
+	pthread_t displayThread;
+	pthread_attr_t attrDisp;
+	//	std::ofstream logfile;
+	std::mutex mutex_d;
+	int posX[5] = {50000,12000,30000,20000,70000};
+	int posY[5] = {19000,12000,7000, 50000, 90000};
 	int posZ[5] = {12000,15000,11000,10000,10000};
-	int map[10][10] = {
-			{0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0}}; // Shrink 100k by 100k map to 10 by 10, each block is 10k by 10k
+	//	int posX[5];
+	//	int posY[5];
+	//	int posZ[5];
+	int map[block_count][block_count]={{0}}; // Shrink 100k by 100k map to 10 by 10, each block is 10k by 10k
+	//	memset(map, 0, sizeof(map[0][0]) * block_count * block_count);
 
-	void test(){
+
+	void printMap(){
 		size_t n = sizeof(posX)/sizeof(int);
-		for(int i=0; i<n; i++){
-			map[posX[i]/SCALER][posY[i]/SCALER]++;
+		//		std::cout << block_count << std::endl;
+		if((int) n != 0){
+			for(int i=0; i<(int) n; i++){
+				map[posX[i]/SCALER][posY[i]/SCALER]++;
+			}
+
+			for(int j=0; j<block_count;j++){
+				for(int k=0; k<block_count;k++){
+					if(map[j][k] == 0)
+						printf("_|");
+					else
+						printf("%d|", map[j][k]);
+
+				}
+				std::cout << std::endl;
+			}
+
+		}
+	}
+
+	void printHeight(){
+		size_t n = sizeof(posZ)/sizeof(int);
+		if((int) n != 0){
+			for(int i =0; i<(int)n;i++){
+				std::cout << "Plane " << std::to_string(i+1) <<
+						" has at height: " << std::to_string(posZ[i])
+						<< std::endl;
+
+			}
 		}
 
-		for(int j=0; j<10;j++){
-			for(int k=0; k<10;k++){
-				printf("%d ", map[j][k]);
-			}
-			std::cout << std::endl;
-		}
 	}
 
 };
