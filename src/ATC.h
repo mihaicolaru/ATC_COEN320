171366c7861c6ebb71015fc9949ac34f2c5e0099
unknown <mihai.c.olaru@gmail.com>
2022-11-12 20:39:43 -0500

    implement input parser, start implement shared memory

--- src/ATC.h
+++ src/ATC.h
@@ -11,15 +11,17 @@
 #include <vector>
 #include <list>
 #include <fstream>
+#include <fcntl.h>
 #include <time.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <errno.h>
 #include <sys/siginfo.h>
 #include <sys/neutrino.h>
+#include <sys/mman.h>
+
 
 #include "Timer.h"
-#include "Display.h"
 
 #define PSR_PERIOD 5000000
 #define SSR_PERIOD 5000000
@@ -32,21 +34,31 @@
 #define SPACE_Z_MAX 25000
 #define SPACE_ELEVATION 15000
 
+struct Plane{
+	int ID;
+	int arrivalTime;
+	int position[3];
+	int speed[3];
+};
+
 class ATC{
 public:
 
 	ATC(){
+		initialize();
 
+		start();
 	}
 
 	~ATC(){
-
+		// release all shared memory pointers
 	}
 
 	int initialize(){
 		// read input from file
 		// initialize shared memory space for each plane
 		// save address pointer to planes vector AND readyPlanes vector
+		readInput();
 		// initialize shared memory space for airspace
 		// initialize plane thread attr struc (if exit on end of exec requires different attr)
 		// initialize thread attr struct (joinable)
@@ -75,9 +87,11 @@ public:
 		return 0;	// set to error code if any
 	}
 
+
+
 	static void *startPlaneUpdate(void *context){
 		// set priority
-		return ((ATC *)context)->updatePlanePosition();
+		return ((ATC *)context)->UpdatePlanePosition();
 	}
 
 	static void *startPSR(void *context){
@@ -114,7 +128,96 @@ public:
 
 protected:
 
-	void* updatePlanePosition(void){
+
+	int readInput(){
+		// open input.txt
+		std::string filename = "./input.txt";
+		std::ifstream input_file_stream;
+
+		input_file_stream.open(filename);
+
+		if (!input_file_stream) {
+			std::cout << "Can't find file input.txt" << std::endl;
+			return 1;
+		}
+
+		int ID, arrivalTime, arrivalCordX, arrivalCordY, arrivalCordZ, arrivalSpeedX, arrivalSpeedY, arrivalSpeedZ;
+
+		std::string separator = " ";
+
+		// parse input.txt
+		while(input_file_stream >> ID >> arrivalTime >>
+				arrivalCordX >> arrivalCordY >> arrivalCordZ >>
+				arrivalSpeedX >> arrivalSpeedY >> arrivalSpeedZ){
+
+			std::cout << ID << separator << arrivalTime << separator << arrivalCordX << separator << arrivalCordY << separator << arrivalCordZ << separator << arrivalSpeedX << separator << arrivalSpeedY << separator << arrivalSpeedZ << std::endl;
+
+			// initialize shared memory space for each plane
+			int fd;
+			Plane* plane_addr;
+
+			// create new shared memory object
+			std::string planeName = "/plane_" + ID;
+			fd = shm_open(planeName.c_str(), O_RDWR | O_CREAT, 0666);
+			if(fd == -1){
+				fprintf(stderr, "Open plane shared memory object failed: %s\n", strerror(errno));
+				return EXIT_FAILURE;
+			}
+
+			// set memory object size
+			if(ftruncate(fd, sizeof(*plane_addr)) == -1){
+				fprintf(stderr, "ftruncate: %s\n", strerror(errno));
+				return EXIT_FAILURE;
+			}
+
+			// map memory object
+			plane_addr = (Plane*)mmap(NULL, sizeof(*plane_addr), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+			if(plane_addr == MAP_FAILED){
+				fprintf(stderr, "mmap failed: %s\n", strerror(errno));
+				return EXIT_FAILURE;
+			}
+
+			// write to shared memory object
+
+
+
+			plane_addr->ID = ID;
+			plane_addr->arrivalTime = arrivalTime;
+			plane_addr->position[0] = arrivalCordX;
+			plane_addr->position[1] = arrivalCordY;
+			plane_addr->position[2] = arrivalCordZ;
+			plane_addr->speed[0] = arrivalSpeedX;
+			plane_addr->speed[1] = arrivalSpeedY;
+			plane_addr->speed[2] = arrivalSpeedZ;
+
+
+
+			std::cout << "Plane: " << plane_addr->ID << "\narrival: " << plane_addr->arrivalTime <<
+					"\nposition: " << plane_addr->position[0] << ", " << plane_addr->position[1] << ", " << plane_addr->position[2] <<
+					"\nspeed: " << plane_addr->speed[0] << ", " << plane_addr->speed[1] << ", " << plane_addr->speed[2] << "\n";
+
+			write(fd, plane_addr, sizeof(*plane_addr));
+
+
+			Plane* plane2;
+
+			ssize_t returnSize = read(fd, plane2, sizeof(*plane2));
+
+			std::cout << "Plane: " << plane2->ID << "\narrival: " << plane2->arrivalTime <<
+					"\nposition: " << plane2->position[0] << ", " << plane2->position[1] << ", " << plane2->position[2] <<
+					"\nspeed: " << plane2->speed[0] << ", " << plane2->speed[1] << ", " << plane2->speed[2] << "\n";
+
+			// add address of shared memory to planes and waiting planes
+			Planes.push_back(plane_addr);
+			waitingPlanes.push_back(plane_addr);
+
+
+		}
+	}
+
+	// ============================ PROCESSES ============================
+
+	void* UpdatePlanePosition(void){
 		// update plane position
 		// checks for airspace limitations
 		// if out of airspace, set plane sm_obj to null, terminate thread
@@ -162,6 +265,30 @@ protected:
 	void* Comm(void){
 		// when notified by computerSystem, sends message m to plane R
 	}
+
+	// ============================ MEMBERS ============================
+
+	// plane queues
+	std::vector<Plane*> Planes;			// all planes shared memory address pointers
+	std::vector<Plane*> waitingPlanes;	// planes not in airspace
+	std::vector<Plane*> airspace;		// planes in airspace
+
+	// timers
+	time_t startTime;
+	time_t endTime;
+
+
+	// threads
+	std::vector<pthread_t> planeThreads;
+	pthread_t primaryRadar;
+	pthread_t secondaryRadar;
+	pthread_t computerSystem;
+	pthread_t display;
+	pthread_t console;
+	pthread_t comm;
+
+	// thread attributes
+	pthread_attr_t detached;				// attribute struct for detached threads
 
 };
 
