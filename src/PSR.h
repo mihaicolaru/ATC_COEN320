c411076a0372a0a9257b3367c345f288b5d51050
minhtrannhat <minhtrannhat@minhtrannhat.com>
2022-11-15 16:23:32 -0500

    Fix(PSR.h): dynamically get the number of planes

--- src/PSR.h
+++ src/PSR.h
@@ -8,18 +8,18 @@
 #ifndef PSR_H_
 #define PSR_H_
 
+#include <fcntl.h>
 #include <fstream>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <vector>
 
-#include "Timer.h"
 #include "Plane.h"
+#include "Timer.h"
 
 #define SIZE 4096
 #define PSR_PERIOD 5000000
@@ -29,155 +29,149 @@ class Plane;
 
 class PSR {
 public:
-	// constructor
-	PSR(int n){
+  // constructor
+  PSR(int numberOfPlanes) { initialize(numberOfPlanes); }
 
+  // destructor
+  ~PSR() {
+    shm_unlink("plane_1");
+    pthread_mutex_destroy(&mutex);
+  }
 
-		initialize(n);
-	}
+  int initialize(int numberOfPlanes) {
+    // set thread in detached state
+    int rc = pthread_attr_init(&attr);
+    if (rc) {
+      printf("ERROR, RC from pthread_attr_init() is %d \n", rc);
+    }
 
-	// destructor
-	~PSR() {
-		shm_unlink("plane_1");
-		pthread_mutex_destroy(&mutex);
-	}
+    rc = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    if (rc) {
+      printf("ERROR; RC from pthread_attr_setdetachstate() is %d \n", rc);
+    }
 
-	int initialize(int n){
-		// set thread in detached state
-		int rc = pthread_attr_init(&attr);
-		if (rc){
-			printf("ERROR, RC from pthread_attr_init() is %d \n", rc);
-		}
+    // open list of waiting planes shm
+    shm_waitingPlanes = shm_open("waiting_planes", O_RDONLY, 0666);
+    if (shm_waitingPlanes == -1) {
+      perror("in shm_open() PSR");
+      exit(1);
+    }
 
-		rc = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-		if (rc){
-			printf("ERROR; RC from pthread_attr_setdetachstate() is %d \n", rc);
-		}
+    ptr_waitingPlanes =
+        mmap(0, 4096, PROT_READ, MAP_SHARED, shm_waitingPlanes, 0);
+    if (ptr_waitingPlanes == MAP_FAILED) {
+      perror("in map() PSR");
+      exit(1);
+    }
 
-		// open list of waiting planes shm
-		shm_waitingPlanes = shm_open("waiting_planes", O_RDONLY, 0666);
-		if(shm_waitingPlanes == -1){
-			perror("in shm_open() PSR");
-			exit(1);
-		}
+    char allPlanes[36];
+    char filename[8];
+    char *arrayOfFilenames[4];
 
-		ptr_waitingPlanes = mmap(0, 4096, PROT_READ, MAP_SHARED, shm_waitingPlanes, 0);
-		if(ptr_waitingPlanes == MAP_FAILED){
-			perror("in map() PSR");
-			exit(1);
-		}
+    for (int i = 0; i < numberOfPlanes * 9; i++) {
+      pthread_mutex_lock(&mutex);
 
-		char allPlanes[36];
-		char filename[8];
-		char* arrayOfFilenames[4];
+      char readChar = *((char *)ptr_waitingPlanes + i);
+      allPlanes[i] = readChar;
 
-		for(int i = 0; i < 36; i++){
-			pthread_mutex_lock(&mutex);
+      //			int j = 0;
+      //			bool endOfLine = false;
+      //
+      //			while(!endOfLine){
+      //
+      //				char readChar =
+      //*((char*)ptr_waitingPlanes + j);
+      //
+      ////				if(readChar == " ")){
+      ////					endOfLine = true;
+      ////				}
+      ////				else{
+      ////					filename[j] = readChar;
+      ////				}
+      //				j++;
+      //			}
+      //
+      //			arrayOfFilenames[i] = strdup(filename);
 
-			char readChar = *((char*)ptr_waitingPlanes + i);
-			allPlanes[i] = readChar;
+      pthread_mutex_unlock(&mutex);
+    }
 
-//			int j = 0;
-//			bool endOfLine = false;
-//
-//			while(!endOfLine){
-//
-//				char readChar = *((char*)ptr_waitingPlanes + j);
-//
-////				if(readChar == " ")){
-////					endOfLine = true;
-////				}
-////				else{
-////					filename[j] = readChar;
-////				}
-//				j++;
-//			}
-//
-//			arrayOfFilenames[i] = strdup(filename);
+    printf("PSR initial read: %s\n", allPlanes);
 
-			pthread_mutex_unlock(&mutex);
-		}
+    return 0;
+  }
 
+  int start() {
+    std::cout << "PSR start called\n";
+    if (pthread_create(&PSRthread, &attr, &PSR::startPSR, (void *)this) !=
+        EOK) {
+      PSRthread = NULL;
+    }
+  }
 
-		printf("PSR initial read: %s\n", allPlanes);
+  int stop() {
+    pthread_join(PSRthread, NULL);
+    return 0;
+  }
 
+  static void *startPSR(void *context) { ((PSR *)context)->operatePSR(); }
 
-		return 0;
-	}
+  void *operatePSR(void) {
+    //		std::cout << "start exec\n";
+    // update position every second from position and speed every second
+    int chid = ChannelCreate(0);
+    if (chid == -1) {
+      std::cout << "couldn't create channel!\n";
+    }
 
-	int start(){
-		std::cout << "PSR start called\n";
-		if(pthread_create(&PSRthread, &attr, &PSR::startPSR, (void* ) this) != EOK){
-			PSRthread = NULL;
-		}
-	}
+    Timer timer(chid);
+    timer.setTimer(PSR_PERIOD, PSR_PERIOD);
 
-	int stop(){
-		pthread_join(PSRthread, NULL);
-		return 0;
-	}
+    int rcvid;
+    Message msg;
 
-	static void *startPSR(void *context){
-		((PSR *)context)->operatePSR();
-	}
+    while (1) {
+      if (rcvid == 0) {
+        //				pthread_mutex_lock(&mutex);
 
-	void* operatePSR(void){
-		//		std::cout << "start exec\n";
-		// update position every second from position and speed every second
-		int chid = ChannelCreate(0);
-		if(chid == -1){
-			std::cout << "couldn't create channel!\n";
-		}
+        //				printf("PSR read: ");
+        //				printf("%s", ptr_waitingPlanes);
+        //				printf("\n");
 
-		Timer timer(chid);
-		timer.setTimer(PSR_PERIOD, PSR_PERIOD);
+        //				pthread_mutex_unlock(&mutex);
+      }
+      rcvid = MsgReceive(chid, &msg, sizeof(msg), NULL);
+      //			std::cout << "executing end\n";
+    }
 
-		int rcvid;
-		Message msg;
+    ChannelDestroy(chid);
 
+    return 0;
+  }
 
-		while(1) {
-			if(rcvid == 0){
-				//				pthread_mutex_lock(&mutex);
-
-//				printf("PSR read: ");
-//				printf("%s", ptr_waitingPlanes);
-//				printf("\n");
-
-				//				pthread_mutex_unlock(&mutex);
-			}
-			rcvid = MsgReceive(chid, &msg, sizeof(msg), NULL);
-			//			std::cout << "executing end\n";
-		}
-
-		ChannelDestroy(chid);
-
-		return 0;
-	}
-
-	int discover() {
-		// scan all planes and find ready (Tarrival <= Tcurrent)
-		// move ready planes to ready list
-	}
+  int discover() {
+    // scan all planes and find ready (Tarrival <= Tcurrent)
+    // move ready planes to ready list
+  }
 
 private:
-	// list of all planes
-	// list of ready planes
+  // list of all planes
+  // list of ready planes
 
-	std::vector<void *> planePtrs;
-	std::vector<std::string> planeFD;
+  std::vector<void *> planePtrs;
+  std::vector<std::string> planeFD;
 
-	pthread_t PSRthread;
-	pthread_attr_t attr;
-	pthread_mutex_t mutex;
+  pthread_t PSRthread;
+  pthread_attr_t attr;
+  pthread_mutex_t mutex;
 
-	time_t at;
-	time_t et;
+  time_t at;
+  time_t et;
 
-	int shm_waitingPlanes;
-	void *ptr_waitingPlanes;
+  int shm_waitingPlanes;
+  void *ptr_waitingPlanes;
 
-	friend class Plane;
+  friend class Plane;
 };
 
 #endif /* PSR_H_ */
